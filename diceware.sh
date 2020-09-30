088381c469907e5ef91056bd8193c91e80478ac2
Jan Sarenik <jajomojo@gmail.com>
2020-09-30 16:15:38 +0200

    diceware.sh: Fix bias
    This is a commit of change that was done earlier (Mar 30 2020),
    but commited just now.

--- diceware.sh
+++ diceware.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/busybox/ash
 
-WL=${2:-"diceware_wordlist_cz.txt"}
+a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd $a; pwd)
+export PATH=/busybox
+#WL=${2:-"$BINDIR/diceware_wordlist_cz.txt"}
+WL=${2:-"$BINDIR/diceware.wordlist.asc"}
+
+  zzz=1
+  zzo=6
+  zoz=2
+  zoo=5
+  ozz=3
+  ozo=4
+  ooz=0
+  ooo=0
 
 test "$1" = "-wl" && {
   WL=$2
@@ -8,11 +20,19 @@ test "$1" = "-wl" && {
   shift
 }
 
-I=${1:-"6"}
+I=${1:-"12"}
+
+getots() {
+  R=$((RANDOM%2))$((RANDOM%2))$((RANDOM%2))
+  R=${R//1/o}
+  R=${R//0/z}
+  eval echo \$$R | grep '[1-6]' || getots
+}
 
 for i in $(seq $I)
 do
-  W=$(grep $((RANDOM%6+1))$((RANDOM%6+1))$((RANDOM%6+1))$((RANDOM%6+1))$((RANDOM%6+1)) $WL | awk '{print $2}')
+  W=$(grep $(getots)$(getots)$(getots)$(getots)$(getots) $WL | awk '{print $2}')
+  #W=$(echo $(getots)$(getots)$(getots)$(getots)$(getots))
   WF="$WF $W"
 done
 

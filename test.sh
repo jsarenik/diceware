3f9c52649d626436d67c67f8d2cb727ce326017a
Jan Sarenik <jajomojo@gmail.com>
2022-06-03 15:38:27 +0200

    test.sh: Simplify and use system ash
    I use to have a static busybox in /busybox/*

--- test.sh
+++ test.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 
-/busybox/timeout 10 /busybox/ash -s </dev/null 2>/dev/null <<EOF
+timeout 10 ash -s </dev/null 2>/dev/null <<EOF
 . $PWD/ozo.inc
-#  zzz=1
-#  zzo=6
-#  zoz=2
-#  zoo=5
-#  ozz=3
-#  ozo=4
-##  ooz=0
-##  ooo=0
-#getots() {
-#  R=\$((RANDOM%2))\$((RANDOM%2))\$((RANDOM%2))
-#  test \${R%%[01]} = "11" && return
-#  R=\${R//1/o}
-#  R=\${R//0/z}
-#  eval echo \\$\$R
-#}
+
 while
   true
 do

11ed03ea9012154151bdc9010a1c0328eba0609a
Jan Sarenik <jajomojo@gmail.com>
2022-06-03 15:04:05 +0200

    Add ozo include file and test.sh
    The idea shall be clear:
    
        $ ./test.sh
         112472 1
         112700 2
         112956 3
         112363 4
         112747 5
         112610 6
        $ ./test.sh
         101740 1
         102384 2
         102416 3
         102333 4
         102333 5
         102529 6
    
    diceware.sh now uses the same ozo.inc file as test.sh

--- diceware.sh
+++ diceware.sh
@@ -5,14 +5,7 @@ export PATH=/busybox
 #WL=${2:-"$BINDIR/diceware_wordlist_cz.txt"}
 WL=${2:-"$BINDIR/diceware.wordlist.asc"}
 
-  zzz=1
-  zzo=6
-  zoz=2
-  zoo=5
-  ozz=3
-  ozo=4
-  ooz=0
-  ooo=0
+. $PWD/ozo.inc
 
 test "$1" = "-wl" && {
   WL=$2
@@ -21,13 +14,6 @@ test "$1" = "-wl" && {
 }
 
 I=${1:-"12"}
-
-getots() {
-  R=$((RANDOM%2))$((RANDOM%2))$((RANDOM%2))
-  R=${R//1/o}
-  R=${R//0/z}
-  eval echo \$$R | grep '[1-6]' || getots
-}
 
 for i in $(seq $I)
 do

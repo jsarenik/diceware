658acfff07f6df7b2511a9f58134882ad45aa064
Jan Sarenik <jajomojo@gmail.com>
2024-07-11 16:19:35 +0200

    misc/test2.sh: simplify

--- misc/test2.sh
+++ misc/test2.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-/busybox/timeout 10 /busybox/ash -se <<EOF
+busybox timeout 10 busybox ash -se <<EOF
 while
   true
 do
   #echo "ibase=16; obase=10; \$(dd if=/dev/urandom bs=1 count=1 2>/dev/null | xxd -p | tr '[a-f]' '[A-F]')%6+1;" | bc
   #dd if=/dev/random bs=1 count=1 2>/dev/null | xxd -p
-  /busybox/dd if=/dev/random bs=1 count=1 2>/dev/null | /busybox/hexdump
+  busybox dd if=/dev/random bs=1 count=1 2>/dev/null | busybox xxd -p | busybox cut -b1
 done > /tmp/test$$
 EOF
 
 sort /tmp/test$$ | uniq -c
+rm -v /tmp/test$$

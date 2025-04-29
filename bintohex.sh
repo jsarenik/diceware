e046af1b812efa87e92100ac72cd1eec767f5ff7
Jan Sarenik <jajomojo@gmail.com>
2025-04-29 12:27:11 +0000

    bintohex 2025 O:-D

--- bintohex.sh
+++ bintohex.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 {
 {
-echo "obase=16; ibase=2; 11"
+echo "obase=16; ibase=2; 1111"
 test "$1" = "" && cat || echo "$@"
-echo 11
 } | tr -d '\n '
 echo
-} | bc | cut -b3- | rev | cut -b3- | rev
+} | bc | cut -b2- | tr -d '\\\n' | grep .
